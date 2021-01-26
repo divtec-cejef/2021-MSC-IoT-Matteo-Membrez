ce7c5677544607a95a2f3df1da5d018c85b5ba03
Matteo Membrez <54532942+membmat@users.noreply.github.com>
2021-01-26 09:10:17 +0100

    Récupération de l'ID et du PAC

---
+++ src/arduino/ID_PAC/ID_PAC.ino
@@ -0,0 +1,29 @@
+#include <SigFox.h>
+#include "ArduinoLowPower.h"
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {};
+
+  if (!SigFox.begin()) {
+    Serial.println("Unable to init the Atmel ATA8520 Sigfox chipset");
+    return;
+  }
+  SigFox.debug();
+
+  String ID = SigFox.ID();
+  String PAC = SigFox.PAC();
+
+  // Display module informations
+  Serial.println("MKRFOX1200 informations");
+  Serial.print("ID\t");
+  Serial.println(SigFox.ID());
+  Serial.print("PAC\t");
+  Serial.println(SigFox.PAC());
+
+  Serial.println("Register your board on https://buy.sigfox.com/activate");
+}
+
+void loop()
+{
+}
