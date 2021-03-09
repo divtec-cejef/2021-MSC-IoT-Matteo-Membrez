3feddf30acf9bc833fec78971c8f007ac44e5f46
Matteo Membrez <54532942+membmat@users.noreply.github.com>
2021-03-09 15:46:06 +0100

    Modifications visuelles

--- src/webProject/view/index.view.php
+++ src/webProject/view/index.view.php
@@ -19,12 +19,12 @@
     <div id="content">
         <div id="temp">
             <p><?php echo $values[0]['nom_salle']; ?> - Température</p>
-            <p><?php echo $values[0]['temperature_message']; ?>°C</p>
+            <p><?php echo $values[0]['temperature_message']; ?> °C</p>
         </div>
 
         <div id="humidity">
             <p><?php echo $values[0]['nom_salle']; ?> - Humidité</p>
-            <p><?php echo $values[0]['humidite_message']; ?>%</p>
+            <p><?php echo $values[0]['humidite_message']; ?> %</p>
         </div>
     </div>
 
@@ -33,17 +33,19 @@
 
         <table>
             <tr>
+                <th>Séquence</th>
                 <th>Device</th>
-                <th>Température</th>
-                <th>Humidité</th>
+                <th>Température (°C)</th>
+                <th>Humidité (%)</th>
                 <th>Date</th>
                 <th>Salle</th>
             </tr>
             <?php foreach ($values as $row) : ?>
             <tr>
+                <td><?php echo $row['seq_num_message']; ?></td>
                 <td><?php echo $row['num_id_capteur']; ?></td>
-                <td><?php echo $row['temperature_message']; ?></td>
-                <td><?php echo $row['humidite_message']; ?></td>
+                <td><?php echo $row['temperature_message']; ?> °C</td>
+                <td><?php echo $row['humidite_message']; ?> %</td>
                 <td><?php echo $row['date_message']; ?></td>
                 <td><?php echo $row['nom_salle']; ?></td>
             </tr>
