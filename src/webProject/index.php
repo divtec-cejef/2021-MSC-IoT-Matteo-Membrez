a6a8c058ea1fa63a80beb996a6033ceb10d8621d
Matteo Membrez <54532942+membmat@users.noreply.github.com>
2021-03-16 13:02:03 +0100

    Modification du site
    Modification du CSS + du Contenu
    Ajout des boutons de modification et de supression

--- src/webProject/index.php
+++ src/webProject/index.php
@@ -6,7 +6,15 @@ require 'inc/conn_inc.php';
 
 // Décode le résultat reçu en format JSON lors de l'appelle de l'API
 // et le stock dans un tableau
-$json = file_get_contents('http://membmat.divtec.me/iot/api/values');
+if(isset($_GET['salle'])) {
+    $json = file_get_contents('http://membmat.divtec.me/iot/api/salles/' . $_GET['salle']);
+} else {
+    $json = file_get_contents('http://membmat.divtec.me/iot/api/values');
+}
+
 $values = json_decode($json, JSON_OBJECT_AS_ARRAY);
+
+// récuperation de l'action passée en get
+$act = filter_input(INPUT_GET, 'act',FILTER_SANITIZE_STRING);
 
 require VIEW_DIR . 'index.view.php';
--- src/webProject/view/index.view.php
+++ src/webProject/view/index.view.php
@@ -16,9 +16,9 @@
         <img src="../img/map.jpg" alt="The map" usemap="#plan">
 
         <map name="plan">
-            <area target="" alt="B1-15" title="B1-15" href="#" coords="20,23,252,281" shape="rect">
-            <area target="" alt="B1-04" title="B1-04" href="#" coords="21,283,252,384" shape="rect">
-            <area target="" alt="B1-13" title="B1-13" href="#" coords="449,282,679,500" shape="rect">
+            <area target="" alt="B1-15" title="B1-15" href="?salle=B1-15" coords="20,23,252,281" shape="rect">
+            <area target="" alt="B1-04" title="B1-04" href="?salle=B1-04" coords="21,283,252,384" shape="rect">
+            <area target="" alt="B1-13" title="B1-13" href="?salle=B1-13" coords="449,282,679,500" shape="rect">
         </map>
     </div>
 
@@ -45,6 +45,8 @@
                 <th>Humidité (%)</th>
                 <th>Date</th>
                 <th>Salle</th>
+                <th>Modifier</th>
+                <th>Supprimer</th>
             </tr>
             <?php foreach ($values as $row) : ?>
             <tr>
@@ -54,6 +56,8 @@
                 <td><?php echo $row['humidite_message']; ?> %</td>
                 <td><?php echo $row['date_message']; ?></td>
                 <td><?php echo $row['nom_salle']; ?></td>
+                <td class="edit"><a href="?act=upd"><img src="../img/editer.png" alt="bouton d'édition" width="40px" height="40px"></a></td>
+                <td class="delete"><a href="?act=del"><img src="../img/delete.png" alt="bouton de supression" width="40px" height="40px"></a></td>
             </tr>
             <?php endforeach; ?>
         </table>
